2cfcf5bb67f6a62f7b5af17cc48b841f09bece40
Philipp Dargel <chisui@fb3.de>
2017-10-16 18:48:50 +0200

    tests

--- test/Spec/SAT/IntSolver.hs
+++ test/Spec/SAT/IntSolver.hs
@@ -1,14 +1,21 @@
 module Spec.SAT.IntSolver where
 
-import Control.Monad.Trans.Class
-    ( lift
-    )
+
+import Control.Monad.Trans.State.Lazy ( StateT )
+import Control.Monad.Trans.Class ( lift )
 
 import SAT.IntSolver
 import SAT.Types
+import SAT.Util
 import Spec.SAT.IntSolver.IPASIR.TH
 
 
 main = evalIntSolver cryptoMiniSat $ do
-    addIntClause ([1, -2, 3] :: [Int])
-    lift . print =<< solve
+    add [ 1, 2]
+    add [-1, 2]
+    printSolve
+    printSolve
+  where
+    add :: [Int] -> StateT CryptoMiniSat IO ()
+    add = addIntClause
+    printSolve = lift . putStr . prettyESolution =<< solve
