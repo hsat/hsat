92e361c2281b3c6f973c15a7b3588e9cbbc070f0
Philipp Dargel <chisui@fb3.de>
2017-10-19 17:35:25 +0200

    doku

--- src/SAT/Types/Lit.hs
+++ src/SAT/Types/Lit.hs
@@ -23,9 +23,10 @@ import Data.Traversable ( Traversable ( traverse ) )
 import Control.Comonad ( Comonad ( extract, duplicate ) )
 
 
+-- | Represents a variable associated with a sign.
 data Lit a
-    = Pos a
-    | Neg a
+    = Pos a -- ^ a positive literal
+    | Neg a -- ^ a negative literal
         deriving (Eq, Functor, Generic)
 
 instance Eq1 Lit where
@@ -69,12 +70,15 @@ instance IsString a => IsString (Lit a) where
     fromString = return . fromString
 
 -- | Create a Literal with given sign
-lit :: Bool -> a -> Lit a
+lit :: Bool -- ^ sign
+    -> a    -- ^ value
+    -> Lit a
 lit True  = Pos
 lit False = Neg
 
 -- | Create an Empty Literal with given sign
-boolToLit :: Bool -> Lit ()
+boolToLit :: Bool -- ^ sign
+          -> Lit ()
 boolToLit = (`lit` ())
 
 -- | Negate a literal.
@@ -83,9 +87,13 @@ negLit (Pos a) = Neg a
 negLit (Neg a) = Pos a
 
 -- | Get the sign of a Literal (True for positive)
+-- @lit (isLitPositive l) (extract l) = l@
 isLitPositive :: Lit a -> Bool
 isLitPositive (Pos _) = True
 isLitPositive (Neg _) = False
+
+litFrom0 :: a -- ^ throws error
+litFrom0 = error "can not create Lit from 0"
 
 instance (Enum a) => Enum (Lit a) where
     -- | Coerces a @Literal@ of a @Num@ @n@ to a signed @Int@ where the sign is the literals sign.
@@ -99,28 +107,38 @@ instance (Enum a) => Enum (Lit a) where
                 then 1
                 else -1
 
-    toEnum 0 = error "can not create Lit from 0"
+    toEnum 0 = litFrom0
     toEnum x = lit (x>0) (toEnum $ abs x)
 
+-- | implemented to be able to create a 'Lit' from a numeric literal. 'fromInteger' fails on @0@.
 instance (Num a) => Num (Lit a) where
     a + b = lit (isLitPositive a || isLitPositive b) (extract a + extract b)
     a * b = lit (isLitPositive a && isLitPositive b) (extract a * extract b)
     abs = Pos . abs . extract
     signum = fmap signum
     negate = negLit . fmap negate
-    fromInteger 0 = error "can not create Lit from 0"
+    fromInteger 0 = litFrom0
     fromInteger i = lit (i > 0) (fromInteger $ abs i)
 
+-- | @a@ may be treated as a @Lit l@.
+-- the following has to hold:
+--
+-- @toLit . fromLit = id@
+--
+-- @fromLit . toLit = id@
+--
+-- 'toLit' may be undefined for values that do not carry a sign, like @0@.
 class IsLit a l where
     toLit :: a -> Lit l
     fromLit :: Lit l -> a
 
 instance IsLit Int Word where
-    toLit 0 = error "can not create Lit from 0"
+    toLit 0 = litFrom0
     toLit i = lit (i > 0) $ toEnum $ (abs i) - 1
     fromLit (Pos i) =  1 + fromEnum i
     fromLit (Neg i) = -1 - fromEnum i
 
+-- | identity
 instance IsLit (Lit a) a where
     toLit   = id
     fromLit = id
